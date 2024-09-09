e14200f115338c7b4c592a2ec15eee1cee26d093
Mahender <varre.mahender@capgemini.com>
2024-09-09 21:45:29 +0530

    implemented Export to Pdf and Export to pdf in All Users List

--- src/app/components/all-users/all-users.component.ts
+++ src/app/components/all-users/all-users.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { User } from '../../models/user';
 import { UserService } from '../../services/user.service';
+import jsPDF from 'jspdf';
+import autoTable from 'jspdf-autotable';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-all-users',
@@ -28,4 +31,47 @@ export class AllUsersComponent implements OnInit {
   convertDateTimeToDateString(date: Date): string {
     return String(date)?.replace(/T.*/, '');
   }
+
+  exportPDF() {
+    const doc = new jsPDF();
+    doc.text("All Users List", 14, 16);
+
+    const tableBody = this.userList.map(usr => [
+      usr.name,
+      usr.email,
+      usr.address.state,
+      usr.balance,
+      usr.createdAt.toString()
+    ]);
+
+    autoTable(doc, {
+      head: [['Name', 'Email', 'Address','Balance', 'Member Since']],
+      body: tableBody,
+      startY: 20,
+      styles: { fontSize: 10 },
+      
+    });
+
+    doc.save('all-users-list.pdf');
+  }
+ 
+  exportExcel() {
+    const tableBody = this.userList.map(usr => [
+      usr.name,
+      usr.email,
+      usr.address.state,
+      usr.balance,
+      usr.createdAt.toString()
+    ]);
+
+    const ws = XLSX.utils.aoa_to_sheet([
+      ['Name', 'Email', 'Address','Balance', 'Member Since'],
+      ...tableBody
+    ]);
+
+    const wb = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'All Users List');
+    XLSX.writeFile(wb, 'all-users-List.xlsx');
+  }
+  
 }
